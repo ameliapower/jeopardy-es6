e998a66f99c3cadc7a4ae27bc0f2690463aa018c
Ames <ames@Lilys-MacBook-Air-2.local>
2018-03-01 08:20:27 -0500

    fetches category data on load, fetches clue data based on user input

--- src/js/index.js
+++ src/js/index.js
@@ -1,56 +1,78 @@
 import '../css/global.scss';
-// import lodash from 'lodash';
 
-function getClues(triviaId) {
-	console.log('Category ID: '+triviaId);
+let categories = 'http://jservice.io/api/categories?&count=100', 
+	questions = "http://jservice.io/api/clues?&category=",
+	// trivia = [], //create holding array
+	cluesDiv = document.getElementById('clues'),
+	jeopardy;
 
-	let url = "http://jservice.io/api/clues?&category="+triviaId;
-	let cluesDiv = document.getElementById('clues');
-	let trivia = []; //create holding array
-	let jeopardy;
 
-	fetch(url)
-		.then(response => response.json()
-			.then(data => {
-				jeopardy='<ul class="list-group">';
-			    for(var i=0; i<data.length; i++) {
-			    	jeopardy += '<li class="list-group-item">'+data[i].question+
-			    	'<input class="answer" type="text" placeholder="enter answer" />'+
-			    	'<span class="hidden correct-answer">'+data[i].answer+
-			    	'</span><span class="hidden value">'+data[i].value+'</span></li>';
-			    }
-			    jeopardy+='</ul>';
-			    cluesDiv.innerHTML = jeopardy;
+let getData = url => {
+	return fetch(url).then(response => response.json())
+		.catch(error => console.error('Error:', error))
+} 
 
-			
-			    var answerBox = document.querySelectorAll('input');
 
-				if(answerBox && answerBox !== null){
-					for(var i=0; i<answerBox.length; i++){
-						answerBox[i].addEventListener('change', e => {
-							// console.log(e.target.nextSibling.innerHTML)
-							if(e.target.value === e.target.nextSibling.innerHTML){
-								alert('correct');
-							}else{
-								alert('wrong');
-							}
-						})
+//GET CATEGORY LIST
+getData(categories).then( data => {
+	// console.log(data)
+	let cats='<ul><h2>Choose a category</h2>';
+	for(let i=0; i<data.length; i++) {
+		cats+='<li>'+data[i].id + ' - ' + data[i].title+'</li>';
+	}
+	cats+='</ul>';
+	document.getElementById('catList').innerHTML = cats;
+});
+
+
+//ON USER INPUT, GET CATEGORY CLUES
+document.getElementById('cat-id').addEventListener('change', e => {
+	//TODO: Change from input box to select
+	if(e.target.value === "" || e.target.value === NaN ){return }
+	let questions = 'http://jservice.io/api/clues?&category='+e.target.value; 
+	
+	getData(questions).then( data => {
+		jeopardy='<ul class="list-group">';
+		for(let i=0; i<data.length; i++) {
+			jeopardy += '<li class="list-group-item">'+data[i].question+
+			'<input class="answer" type="text" placeholder="enter answer" />'+
+			'<span class="hidden correct-answer">'+data[i].answer+
+			'</span><span class="hidden value">'+data[i].value+'</span></li>';
+		}
+		jeopardy+='</ul>';
+		cluesDiv.innerHTML = jeopardy;
+
+		//CHECK ANSWERS ARE CORRECT
+		let answerBox = document.querySelectorAll('.answer');
+
+		if(answerBox && answerBox !== null){
+			for(let i=0; i<answerBox.length; i++){
+				answerBox[i].addEventListener('change', e => {
+					let correctAnswer = e.target.nextSibling.innerHTML;
+					// console.log('e.target.value');
+					// console.log(e.target.value);
+					if(e.target.value && e.target.value === correctAnswer){
+						alert('correct');
+					}else{
+						alert('wrong');
 					}
-				}
-	    	
+				})
+			}
 
-		} //then
-	)); //first, second then
-	//fetch
+		//TO DO: limit tries
+		//TO DO: add values to score
+
+		} //check answer
+
+	}) //getData
+}) //eventListener
 
 
 
-} //getClues
 
-getClues(1212); 
 
-// choose random cat id
-// id must be max 4 numbers
+// choose cat from left rail by selection
+// id must be max 5 numbers
 // two chances to get correct
 // if correct value is added to score
 // otherwise next question is enabled
