ba8e0729b0bbff4f3b37f980b17117a525fbfe3e
Ames <ames@Lilys-MacBook-Air-2.local>
2018-03-11 20:44:16 -0400

    check answers, scores added, some styling

--- src/js/index.js
+++ src/js/index.js
@@ -1,84 +1,116 @@
-import '../css/global.scss';
+import '../css/global.scss'
 
-let categories = 'http://jservice.io/api/categories?&count=100', 
+"use strict"
+
+let categories = 'http://jservice.io/api/categories?&count=50', 
 	questions = "http://jservice.io/api/clues?&category=",
-	// trivia = [], //create holding array
 	cluesDiv = document.getElementById('clues'),
+	totalDiv = document.getElementById('total'),
+	catList = document.getElementsByTagName('li'),
 	jeopardy;
-
+	
 
 let getData = url => {
-	return fetch(url).then(response => response.json())
-		.catch(error => console.error('Error:', error))
+	return fetch(url)
+		.then(response => response.json())
+		.catch(error => 
+			console.error('Error:', error)
+		)
 } 
 
 
-//GET CATEGORY LIST
+//GET CATEGORIES LIST ON LOAD
 getData(categories).then( data => {
-	// console.log(data)
-	let cats='<ul><h2>Choose a category</h2>';
-	for(let i=0; i<data.length; i++) {
-		cats+='<li>'+data[i].id + ' - ' + data[i].title+'</li>';
+	var cats='<ul><h2>Choose a category</h2>';
+	for(let i=0; i<data.length; i++){
+		cats+='<li><span class="hidden">'+data[i].id + '</span><span>' + lowerCaseIt(data[i].title)+'</span></li>'
 	}
-	cats+='</ul>';
-	document.getElementById('catList').innerHTML = cats;
+	cats+='</ul>'
+	document.getElementById('catList').innerHTML = cats
+	for(var i=0; i<catList.length; i++){
+		catList[i].onclick = getClues
+	}
 });
 
 
-//ON USER INPUT, GET CATEGORY CLUES
-document.getElementById('cat-id').addEventListener('change', e => {
-	//TODO: Change from input box to select
-	if(e.target.value === "" || e.target.value === NaN ){return }
-	let questions = 'http://jservice.io/api/clues?&category='+e.target.value; 
+function lowerCaseIt(string){
+	return string.toLowerCase()
+}
+
+
+//GET LIST OF CLUES ON SELECT
+function getClues(){
+	let selectedCatId = this.querySelector('span').innerText,
+		selectedName = this.querySelector('span:nth-child(2n)').innerText,
+		questions = 'http://jservice.io/api/clues?&category='+selectedCatId,
+		clueHeader = document.getElementById('clue-header'),
+		total = 0; 
+
+	this.classList.toggle('selected')
+	clueHeader.innerText = lowerCaseIt(selectedName)
 	
 	getData(questions).then( data => {
-		jeopardy='<ul class="list-group">';
+		jeopardy='<ul class="list-group">'
 		for(let i=0; i<data.length; i++) {
 			jeopardy += '<li class="list-group-item">'+data[i].question+
-			'<input class="answer" type="text" placeholder="enter answer" />'+
+			'<input class="answer" type="text" placeholder="Enter answer" />'+
 			'<span class="hidden correct-answer">'+data[i].answer+
-			'</span><span class="hidden value">'+data[i].value+'</span></li>';
+			'</span><span class="hidden points">'+data[i].value+'</span></li>';
 		}
 		jeopardy+='</ul>';
 		cluesDiv.innerHTML = jeopardy;
 
 		//CHECK ANSWERS ARE CORRECT
 		let answerBox = document.querySelectorAll('.answer');
-
+		
 		if(answerBox && answerBox !== null){
 			for(let i=0; i<answerBox.length; i++){
+				let attempts=0;
+				answerBox[i].addEventListener('focus', e => {
+					e.target.removeAttribute('placeholder')
+				})
+				answerBox[i].addEventListener('blur', e => {
+					e.target.setAttribute('placeholder', "Enter answer")
+				})
 				answerBox[i].addEventListener('change', e => {
-					let correctAnswer = e.target.nextSibling.innerHTML;
-					// console.log('e.target.value');
-					// console.log(e.target.value);
+					let correctAnswer = e.target.nextSibling.innerText,
+						feedBack = document.createElement('div'),
+						notCorrectAns = e.target.parentNode.querySelector('.incorrect')
+
+					attempts++;	
 					if(e.target.value && e.target.value === correctAnswer){
-						alert('correct');
+						feedBack.classList.add('correct')
+						if(notCorrectAns) notCorrectAns.classList.add('hidden')
+						feedBack.innerText='Correct! You\'ve won '+data[i].value+' points'
+						answerBox[i].parentNode.appendChild(feedBack)
+						total+=data[i].value
+						if(total>0) totalDiv.innerText='Total points: '+total
 					}else{
-						alert('wrong');
+						feedBack.classList.add('incorrect')
+						if(attempts > 1) {
+							notCorrectAns.classList.add('hidden')
+							feedBack.innerText = 'No more chances!'
+							answerBox[i].disabled = true
+							answerBox[i].parentNode.classList.add('disabled')
+							attempts=0
+						}else{
+							feedBack.innerText = 'Try once more'
+						}
+						answerBox[i].parentNode.appendChild(feedBack)
 					}
 				})
 			}
-
-		//TO DO: limit tries
-		//TO DO: add values to score
-
 		} //check answer
-
 	}) //getData
-}) //eventListener
+} //getClues
 
 
 
 
-
-// choose cat from left rail by selection
-// id must be max 5 numbers
-// two chances to get correct
-// if correct value is added to score
-// otherwise next question is enabled
-// questions have to be answered in sequence
-
+// questions have to be answered in sequence?
 // bonus 20 points if answer begins with 'what is...'
+
+
 
 
 
